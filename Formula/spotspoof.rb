879c4ce55ba8e6aa46fc2b0ffaf11725ca259a26
Rob <robertnpage8@gmail.com>
2026-01-04 00:09:57 +0000

    bumper version number & updated hash for new SS release

--- Formula/spotspoof.rb
+++ Formula/spotspoof.rb
@@ -1,8 +1,8 @@
 class Spotspoof < Formula
   desc "Domain spoofing detection for security automation workflows"
   homepage "https://github.com/slimpagey/spotspoof-cli"
-  url "https://github.com/slimpagey/spotspoof-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "e7dc7a36c834ff7a0c43c3789c561f786b488bd2a0b59ee270a58a5e224392e6"
+  url "https://github.com/slimpagey/spotspoof-cli/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "902f0a684047fd4c13221913d38307d8dcbf68e0465d5ac01bcff1a068488073"
   license "MIT"
 
   depends_on "rust" => :build
